d6d95b533ff51ed08e14063ef01f0d2359b36652
Yehuda Heller <116564740+yehudaheller@users.noreply.github.com>
2023-12-28 14:11:16 +0200

    add local db

--- data_layer.py
+++ data_layer.py
@@ -1,23 +1,43 @@
 # data_layer.py
-from random import uniform
+import sqlite3
 
 def load_currency_data():
-    # Load data into Currency table (for simplicity, hardcoded values)
-    return [
-        {"Country": "United States", "Name": "Dollar", "Abbreviation": "USD"},
-        {"Country": "Eurozone", "Name": "Euro", "Abbreviation": "EUR"},
-        {"Country": "Japan", "Name": "Yen", "Abbreviation": "JPY"},
-        {"Country": "Israel", "Name": "Shekel", "Abbreviation": "ILS"},  # Added for ILS
-    ]
+    # Connect to SQLite database
+    conn = sqlite3.connect("stock_exchange.db")
+    cursor = conn.cursor()
+
+    # Fetch data from Currency table
+    cursor.execute("SELECT * FROM Currency")
+    currency_data = cursor.fetchall()
+
+    # Close database connection
+    conn.close()
+
+    return [{"Country": country, "Name": name, "Abbreviation": abbreviation} for country, name, abbreviation in currency_data]
 
 def load_currency_pair_data():
-    # Load data into CurrencyPair table (for simplicity, hardcoded values)
-    return [
-        {"Pair": "USD/EUR", "MinValue": 1.1, "MaxValue": 1.2},
-        {"Pair": "USD/JPY", "MinValue": 100, "MaxValue": 110},
-        {"Pair": "USD/ILS", "MinValue": 3.5, "MaxValue": 3.8},  # Added for USD/ILS
-    ]
+    # Connect to SQLite database
+    conn = sqlite3.connect("stock_exchange.db")
+    cursor = conn.cursor()
+
+    # Fetch data from CurrencyPair table
+    cursor.execute("SELECT * FROM CurrencyPair")
+    currency_pair_data = cursor.fetchall()
+
+    # Close database connection
+    conn.close()
+
+    return [{"Pair": pair, "MinValue": min_value, "MaxValue": max_value} for pair, min_value, max_value in currency_pair_data]
 
 def save_currency_pair_data(pair_data):
-    # Save data (for simplicity, print instead of using a database)
-    print(f"Saved: {pair_data}")
+    # Connect to SQLite database
+    conn = sqlite3.connect("stock_exchange.db")
+    cursor = conn.cursor()
+
+    # Save data to CurrencyPair table
+    cursor.executemany("INSERT OR REPLACE INTO CurrencyPair (Pair, MinValue, MaxValue) VALUES (?, ?, ?)",
+                      [(pair["Pair"], pair["MinValue"], pair["MaxValue"]) for pair in pair_data])
+
+    # Commit changes and close database connection
+    conn.commit()
+    conn.close()
